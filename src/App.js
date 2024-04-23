3440aba6e7ef0adb7cddeccd975314aa50b4d3ca
Emmanuel Fierro <fier9958@bears.unco.edu>
2024-04-23 12:57:12 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './cat.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Image Sentiment Analysis!
+        </p>
+        <a
+          className="App-link"
+          href="https:/google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
